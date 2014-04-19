331ef2b79cf53647a904c4687b0e88e1dfee9e9d
Jeffrey Johnson <jeffcjohnson@live.com>
2014-04-19 00:39:45 -0700

    Input stuff

---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class State {
+	public static int money = 0;
+	public enum Action {
+		Dig = 0,
+		Building = 1,
+		Wall = 2,
+		Generator = 3
+	};
+	public static Action action = Action.Dig;
+	public static Vector2 selectedGrid = new Vector2(0, 0);
+}
---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class UI : MonoBehaviour {
+	public Texture2D Building;
+	public Texture2D Wall;
+	public Texture2D Generator;
+	public Texture2D Dig;
+
+	void OnGUI() {
+		if(Input.GetJoystickNames().Length == 0) {
+			if(GUI.Button(new Rect(10, 10, 100, 100), Building)) {
+			}
+			if(GUI.Button(new Rect(120, 10, 100, 100), Wall)) {
+			}
+			if(GUI.Button(new Rect(230, 10, 100, 100), Generator)) {
+			}
+			if(GUI.Button(new Rect(340, 10, 100, 100), Dig)) {
+			}
+		}
+	}
+	void Update() {
+		if(Input.GetButtonDown("A")) {
+		}
+		if(Input.GetButtonDown("B")) {
+		}
+		if(Input.GetButtonDown("X")) {
+		}
+		if(Input.GetButtonDown("Y")) {
+		}
+		if(Input.GetKeyDown("mouse 0")) {
+			RaycastHit hit;
+			if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit)) {
+			}
+		}
+	}
+}
