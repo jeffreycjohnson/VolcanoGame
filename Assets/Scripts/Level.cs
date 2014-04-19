5da4a73fb17e9ed7b4eda09ec0de842c53994655
patrickgh3 <patrickjtraynor@gmail.com>
2014-04-19 01:00:50 -0700

    Partial level generation tweaks

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -24,23 +24,27 @@ public class Level : MonoBehaviour {
             _tiles[x] = new GameObject[Height];
         }
 
-        Vector3 origin = gameObject.transform.position;
-        //float radius = volcanoradius;
-        //float dradius = -(radius* 0.5f) / Height;
         // slices of the cone
         for (int y = 0; y < Height; y++)
         {
-            float tiley = origin.y + ((float)y * (volcanoheight / Height));
+            float tiley = ((float)y * (volcanoheight / Height));
             float radius = volcanoradius - ((volcanoradius - volcanotopradius) / Height) * y;
+            float scalar = 1 + 1.4f * (Height - y) / Height;
             // create each tile of the slice
             for (int x = 0; x < Width; x++)
             {
-                float theta = (Mathf.PI * 2f / (float)Width) * x;
                 GameObject tile = (GameObject)Instantiate(TileTemplate);
                 tile.gameObject.transform.parent = gameObject.transform;
+                float theta = (Mathf.PI * 2f / (float)Width) * x;
                 tile.transform.position = new Vector3(Mathf.Cos(theta) * radius, tiley, Mathf.Sin(theta) * radius);
+                tile.transform.localScale *= 1.75f;
+                tile.transform.localScale *= scalar;
+                //tile.transform.RotateAround(gameObject.transform.position - (gameObject.transform.position + new Vector3(0, 1, 0)), theta);
+                //tile.transform.position += new Vector3(0, tiley, 0);
                 //tile.GetComponent<Tile>().InitializeLevelData(x, y, gameObject);
-                // TODO: set angle, and maybe scale?
+                tile.transform.Rotate(tile.transform.position - (tile.transform.position + new Vector3(1, 0, 0)), volcanoheight / (volcanoradius - volcanotopradius) * 360, Space.Self);
+                tile.transform.Rotate(tile.transform.position - (tile.transform.position + new Vector3(0, 1, 0)), theta * 360 / (Mathf.PI * 2), Space.Self);
+                _tiles[x][y] = tile;
             }
         }
 	}
