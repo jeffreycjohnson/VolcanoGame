753a8e016d64e94ec150a246bfefa1b75945d3ae
patrickgh3 <patrickjtraynor@gmail.com>
2014-04-19 00:12:42 -0700

    Level tile creation

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -4,30 +4,53 @@ using System.Collections;
 public class Level : MonoBehaviour {
 
     public GameObject TileTemplate;
-    public int width = 20;
-    public int height = 20;
+    public int Width = 20;
+    public int Height = 12;
+    public float volcanoheight = 25;
+    public float volcanoradius = 14;
+    public float volcanotopradius = 5;
     GameObject[][] _tiles;
 
-	// Use this for initialization
+    public static int LevelWidth;
+    public static int LevelHeight;
+
 	void Start ()
     {
-        _tiles = new GameObject[width][];
-        for (int i = 0; i < width; i++)
+        LevelWidth = Width;
+        LevelHeight = Height;
+        _tiles = new GameObject[Width][];
+        for (int x = 0; x < Width; x++)
         {
-            _tiles[i] = new GameObject[height];
+            _tiles[x] = new GameObject[Height];
         }
-        //GameObject tile = (GameObject)Instantiate(TileTemplate);
-        //tile.gameObject.transform.parent = gameObject.transform;
-        //tile.GetComponent<Tile>().SetLevel(gameObject); // TODO: give tile a reference of the level.
+
+        Vector3 origin = gameObject.transform.position;
+        //float radius = volcanoradius;
+        //float dradius = -(radius* 0.5f) / Height;
+        // slices of the cone
+        for (int y = 0; y < Height; y++)
+        {
+            float tiley = origin.y + ((float)y * (volcanoheight / Height));
+            float radius = volcanoradius - ((volcanoradius - volcanotopradius) / Height) * y;
+            // create each tile of the slice
+            for (int x = 0; x < Width; x++)
+            {
+                float theta = (Mathf.PI * 2f / (float)Width) * x;
+                GameObject tile = (GameObject)Instantiate(TileTemplate);
+                tile.gameObject.transform.parent = gameObject.transform;
+                tile.transform.position = new Vector3(Mathf.Cos(theta) * radius, tiley, Mathf.Sin(theta) * radius);
+                //tile.GetComponent<Tile>().InitializeLevelData(x, y, gameObject);
+                // TODO: set angle, and maybe scale?
+            }
+        }
 	}
 	
-	// Update is called once per frame
 	void Update ()
     {
 	
 	}
 
-    GameObject GetTile(int x, int y)
+    public GameObject GetTile(int x, int y)
     {
         return _tiles[x][y];
     }
